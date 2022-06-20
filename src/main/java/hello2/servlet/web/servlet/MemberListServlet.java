d24812e3ab96e49c0de141eedba105247095b733
lby132 <quddbscjswow@naver.com>
2022-06-21 00:14:28 +0900

    jsp세팅

--- src/main/java/hello2/servlet/web/servlet/MemberListServlet.java
+++ src/main/java/hello2/servlet/web/servlet/MemberListServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members")
@@ -19,19 +19,37 @@ public class MemberListServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        final Enumeration<String> e = request.getParameterNames();
-        while (e.hasMoreElements()) {   // 가져온 파라미터 이름들을 루프를 돌려서 전부 뽑아낸다.
-            final String paramName = e.nextElement();   // 파라미터 이름들을 하나씩 담는다.
-            System.out.println("name = " + paramName);  // 파라미터 이름이 출력된다.
-            final String parameter = request.getParameter(paramName);   // 파라미터에 루프를 돌린 이름들을 넣고 value를 가져온다.
-            System.out.println("parameter = " + parameter);     // 요청 파라미터의 이름이 아닌 값들이 출력된다.
-        }
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
 
-        final String[] parameterValues = request.getParameterValues("username");    // username으로 된 파라미터들의 value들을 전부 가져온다.
-        for (String parameterValue : parameterValues) {
-            System.out.println("parameterValue = " + parameterValue);
-        }
         final List<Member> members = memberRepository.findAll();
 
+        final PrintWriter w = response.getWriter();
+        w.write("<html>");
+        w.write("<head>");
+        w.write("    <meta charset=\"UTF-8\">");
+        w.write("    <title>Title</title>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("<a href=\"/index.html\">메인</a>");
+        w.write("<table>");
+        w.write("    <thead>");
+        w.write("    <th>id</th>");
+        w.write("    <th>username</th>");
+        w.write("    <th>age</th>");
+        w.write("    </thead>");
+        w.write("    <tbody>");
+        for (Member member : members) {
+            w.write("    <tr>");
+            w.write("           <td>" + member.getId() + "</td>");
+            w.write("              <td>" + member.getUsername() + "</td>");
+            w.write("              <td>" + member.getAge() + "</td>");
+            w.write("    </tr>");
+        }
+
+        w.write("   </tbody>");
+        w.write("</table>");
+        w.write("</body>");
+        w.write("</html>");
     }
 }
